bda3b7c53961cc2fc2c1fb1e67c2af79a629bc45
z2vvqx <z2vvqx@gmail.com>
2026-08-02 23:03:42 +0200

    fix(discord): use canonical feedback separator

--- discord/discord-bot/src/test/java/net/valoury/discord/bot/DiscordBotConstantsTest.java
+++ discord/discord-bot/src/test/java/net/valoury/discord/bot/DiscordBotConstantsTest.java
@@ -8,8 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class DiscordBotConstantsTest {
     @Test
     void feedbackUsesCanonicalStatusPrefixes() {
-        assertEquals("<a:valorV:1533039959651516498> - ", DiscordBotConstants.SUCCESS_FEEDBACK_PREFIX);
-        assertEquals("<a:valorX:1533039960708350032> - ", DiscordBotConstants.FAILURE_FEEDBACK_PREFIX);
+        assertEquals("<a:valorV:1533039959651516498> ▸ ", DiscordBotConstants.SUCCESS_FEEDBACK_PREFIX);
+        assertEquals("<a:valorX:1533039960708350032> ▸ ", DiscordBotConstants.FAILURE_FEEDBACK_PREFIX);
         assertTrue(DiscordBotConstants.LINK_ALREADY_LINKED.startsWith(
                 DiscordBotConstants.FAILURE_FEEDBACK_PREFIX
         ));
